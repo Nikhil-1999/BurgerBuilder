e717a47ae761e501d7f7356ed6bbd3b733e7db2b
NIKHIL SHARMA <sharmaniksns1999@gmail.com>
2020-08-15 16:07:54 +0530

    Updates

---
+++ precache-manifest.b551c4da6d351df58db7cfa0cecc52e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d60e28e14a578255b9712a63cc02d2",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "b3ef057e7ebcb8963238",
+    "url": "/BurgerBuilder/static/css/main.829c4697.chunk.css"
+  },
+  {
+    "revision": "743d24c9a2302ed6978d",
+    "url": "/BurgerBuilder/static/js/2.763e008e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerBuilder/static/js/2.763e008e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ef057e7ebcb8963238",
+    "url": "/BurgerBuilder/static/js/main.9107fd19.chunk.js"
+  },
+  {
+    "revision": "9b18c247d326418ef3ec",
+    "url": "/BurgerBuilder/static/js/runtime-main.baf6b526.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
